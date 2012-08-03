fce3984898d9feba478422c41d3154e74737bbab
LiuYong <jayxjadmin@gmail.com>
2012-08-03 22:40:06 +0800

    添加注释信息
    Signed-off-by: LiuYong <jayxjadmin@gmail.com>

--- Test01.java
+++ Test01.java
@@ -2,6 +2,7 @@ public class Test01{
 	public static void main(String args[]){
 		int score;
 		score = 100;
+		//注意IF结构的顺序为从大到小，因为IF语句执行完毕后会跳出结构，故可省略对最大值范围的判断
 		if(score > 100 || score < 0){
 			System.out.println("输入错误！");
 			System.out.println("错误值："+score);
