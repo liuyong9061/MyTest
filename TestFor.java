34b3092100ac47792ed84d62209924ca29e8bb08
LiuYong <jayxjadmin@gmail.com>
2012-08-04 19:09:57 +0800

    For循环练习
    Signed-off-by: LiuYong <jayxjadmin@gmail.com>

---
+++ TestFor.java
@@ -0,0 +1,9 @@
+public class TestFor{
+	public static void main(String args[]){
+		System.out.println("进入FOR循环体！");
+		for(int i = 0; i <10; i++){
+			System.out.println(i);
+		}
+		System.out.println("退出FOR循环体");
+	}
+}
