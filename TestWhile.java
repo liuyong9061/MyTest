08803b0887018ff1a0a66984858741f2912a263a
LiuYong <jayxjadmin@gmail.com>
2012-08-04 20:01:58 +0800

    While实例
    Signed-off-by: LiuYong <jayxjadmin@gmail.com>

---
+++ TestWhile.java
@@ -0,0 +1,11 @@
+public class TestWhile{
+	public static void main(String args[]){
+		int i = 0;
+		System.out.println("进入While循环体");
+		while(i < 10){
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("退出While循环体");
+	}
+}
