16eb0ea6bba1f6c6057a12a03d67d2af62c01c7e
LiuYong <jayxjadmin@gmail.com>
2012-08-04 20:04:35 +0800

    While打印偶数
    Signed-off-by: LiuYong <jayxjadmin@gmail.com>

--- TestWhile.java
+++ TestWhile.java
@@ -1,10 +1,11 @@
 public class TestWhile{
 	public static void main(String args[]){
-		int i = 0;
+		int i = 2;
 		System.out.println("进入While循环体");
+		//打印10以内的偶数
 		while(i < 10){
 			System.out.println(i);
-			i++;
+			i = i+2;
 		}
 		System.out.println("退出While循环体");
 	}
