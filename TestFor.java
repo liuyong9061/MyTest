7c590cacdd2fb59ab5c0b47f649a961056329734
LiuYong <jayxjadmin@gmail.com>
2012-08-04 19:15:14 +0800

    For循环打印偶数
    Signed-off-by: LiuYong <jayxjadmin@gmail.com>

--- TestFor.java
+++ TestFor.java
@@ -1,7 +1,8 @@
 public class TestFor{
 	public static void main(String args[]){
 		System.out.println("进入FOR循环体！");
-		for(int i = 0; i <10; i++){
+		//For循环打印10以内的偶数		
+		for(int i = 2; i <= 10; i = i+2){
 			System.out.println(i);
 		}
 		System.out.println("退出FOR循环体");
