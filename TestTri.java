76f6dd1c6a0083772d7424b56dd6a8990a7eb05f
LiuYong <jayxjadmin@gmail.com>
2012-09-13 13:24:44 +0800

    完善三角形代码
    Signed-off-by: LiuYong <jayxjadmin@gmail.com>

--- TestTri.java
+++ TestTri.java
@@ -1,14 +1,18 @@
 public class TestTri{
 	public static void main(String args[]){
-		int n = 10;//程序改进，可自定义三角行数
-		for(int i = 1;i <= n;i++){
-			for(int j = n;j >= i+1;j--){
-				System.out.print(" ");
+		int n = 41;//程序改进，可自定义三角行数
+		if(n <= 40){
+			for(int i = 1;i <= n;i++){
+				for(int j = n;j >= i+1;j--){
+					System.out.print(" ");
+				}
+				for(int k = 0;k < i;k++){
+					System.out.print("* ");
+				}
+				System.out.print("\n");
 			}
-			for(int k = 0;k < i;k++){
-				System.out.print("* ");
-			}
-			System.out.print("\n");
+		}else{
+			System.out.println("传入数值大于显示尺寸！");
 		}
 	}
 }
